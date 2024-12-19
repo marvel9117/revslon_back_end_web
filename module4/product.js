f5d394b145e537d748db6c19308acfbc3179092c
Marvel Paul <djmarvel@yahoo.com>
2024-12-19 02:13:52 -0500

    product management system

---
+++ module4/product.js
@@ -0,0 +1,7 @@
+// product management system that allows adding, viewing, updating,
+//       and deleting product information in a MongoDB database. 
+//       Include features for searching products
+
+
+
+
